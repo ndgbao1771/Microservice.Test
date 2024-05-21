acc805a4c95ad1403f6e096621261c79a3be7afc
Bao Nguyen <bao.nguyen@siliconstack.com.au>
2024-05-21 17:25:51 +0700

    update

--- SchoolAPIGateway/Program.cs
+++ SchoolAPIGateway/Program.cs
@@ -1,16 +1,21 @@
 using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
-builder.Services.AddEndpointsApiExplorer();
-
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
 app.MapControllers();
+
+await app.UseOcelot();
 
 app.Run();
